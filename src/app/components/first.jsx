48dc8692f9c6dcb6989eec289b7bb926aa278809
25lp5076 User <25lp5076@administrators-iMac.local>
2026-01-22 13:49:48 +0800

    beginning

---
+++ src/app/components/first.jsx
@@ -0,0 +1,40 @@
+<div className="flex flex-col justify-between">
+  <div className="flex flex-col gap-2">
+    <div className="flex w-full">
+      <h classname="font-semibold text-sm">First name</h>
+      <h className="font-semibold text-sm text-[#E14942]">*</h>
+    </div>
+    <input
+      className="w-full h-11 rounded-lg border p-3"
+      placeholder="First Name"
+    ></input>
+
+    <div className="flex flex-col gap-2">
+      <div className="flex w-full">
+        <h classname="font-semibold text-sm">Last name</h>
+        <h className="font-semibold text-sm text-[#E14942]">*</h>
+      </div>
+      <input
+        className="w-full h-11 rounded-lg border p-3"
+        placeholder="Last Name"
+      ></input>
+    </div>
+
+    <div className="flex flex-col gap-2">
+      <div className="flex w-full">
+        <h classname="font-semibold text-sm">Username</h>
+        <h className="font-semibold text-sm text-[#E14942]">*</h>
+      </div>
+      <input
+        className="w-full h-11 rounded-lg border p-3"
+        placeholder="Username"
+      ></input>
+    </div>
+  </div>
+  <div>
+    <button className="bg-[#121316] w-full text-base text-white py-2.5 px-3 rounded-md">
+      Continue 1/3 &gt;
+    </button>
+  </div>
+</div>;
+//input oruulaad bolson bol border color tsenher bolno
---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from "react";
+
+export default function Home() {
+  return (
+    <div className="m-auto relative top-45.5 w-120 h-full rounded-lg p-8 bg-white gap-7">
+      <div className="flex flex-col h-32.25 gap-2 justify-start">
+        <img src="/pinecone.png" className="w-15 h-15" />
+        <h1 className="font-semibold text-6.5 text-[#202124] text-shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)]">
+          Join Us! 😎
+        </h1>
+        <p className="text-lg text-[#8E8E8E] ">
+          Please provide all current information accurately.
+        </p>
+      </div>
+      <div className="flex flex-col justify-between">
+        <div></div>
+      </div>
+    </div>
+  );
+}
